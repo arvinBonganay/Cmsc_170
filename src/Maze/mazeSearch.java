e4365c50b14adbe6120cc6172ca2b2cf1c593d1d
Arvin <arvinb450@gmail.com>
2017-10-21 15:53:15 +0800

    update

--- src/Maze/mazeSearch.java
+++ src/Maze/mazeSearch.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 
 public class MazeSearch {
-    
+    // edit later
     
     public static void main(String[] args) {
         String heuristic = "manhattan";
