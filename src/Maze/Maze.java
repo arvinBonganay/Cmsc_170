f02c4c184d7d26d7886b3010589c31867dc125ce
arvin bonganay <arvinb450@gmail.com>
2017-11-01 06:47:27 +0800

    Update Maze.java

--- src/Maze/Maze.java
+++ src/Maze/Maze.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Maze {
     List<List <Square>> maze;
